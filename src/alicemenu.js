a39e755c81a0a8861cfee0f1471f6036c165dbab
liceol <78104845+liceol@users.noreply.github.com>
2021-02-19 09:36:36 -0400

    Add files via upload

---
+++ src/alicemenu.js
@@ -0,0 +1,17 @@
+const alicemenu = (prefix, pushname) => {
+    return 
+   `
+   ╔═══════✮ • ★════════╗   
+   ║ ◪ *Comandos da alice*   
+   ║ ━━━━━━━✮ • ★━━━━━━━ 
+   ║ │
+   ║ ├─ ❏ ${prefix}setprefix
+   ║ ├─ ❏ ${prefix}block
+   ║ ├─ ❏ ${prefix}bc
+   ║ ├─ ❏ ${prefix}bcgc
+   ║ └─ ❏ ${prefix}clearall
+   ╚════════════════════╝`
+
+}
+
+exports.alicemenu = alicemenu
